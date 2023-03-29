bc99e8632f378a8cc6ed152cce6d727a8a498938
PrithiveeRaja <99066041+PrithiveeRaja@users.noreply.github.com>
2023-03-29 23:30:25 +0530

    day 3 assignment submitted

---
+++ day 1 Assignment.java
@@ -0,0 +1,21 @@
+ class Assignment {
+    public static void main(String[] args){
+        System.out.println("Enter a number");
+        double num = -20;
+        if(num>0)
+        {
+            System.out.println("The number is positive");
+        }
+        if(num<0)
+        {
+            System.out.println("The negative");
+        }
+        if(num==0)
+        {
+            System.out.print("The number is zero");
+        }
+
+    }
+}
+    
+
